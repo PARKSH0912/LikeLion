33c0f758a31dc8e880cfbdb54f4d907dfe1b4c80
PARKSH0912 <kkqwert882950@gmail.com>
2025-02-21 15:04:54 +0900

    Loading

--- Loading/Loading/Program.cs
+++ Loading/Loading/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Loading
@@ -10,6 +11,25 @@ namespace Loading
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("□□□□");
+            Thread.Sleep(2000); // 1000 1초 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■□□□");
+            Thread.Sleep(2000); // 1000 1초 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■□□");
+            Thread.Sleep(2000); // 1000 1초 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■□");
+            Thread.Sleep(2000); // 1000 1초 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("■■■■");
+            Thread.Sleep(2000); // 1000 1초 (1000밀리초)
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("텍스트 엘든링");
+
+            Console.WriteLine("스토리1");
         }
     }
 }
