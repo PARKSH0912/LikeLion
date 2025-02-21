2c8889629de564446c1ffce292b3cc363325f90d
PARKSH0912 <kkqwert882950@gmail.com>
2025-02-21 10:22:52 +0900

    Study2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수선언
+            int age; //정수형 변수 
+            age = 25; // 변수에 값 저장 
+
+            Console.WriteLine(age); // 변수에 저장된 값을 출력 // WriteLine 변수값이후 엔터를 넣어줌 
+
+
+            // 컨트롤 + s = 저장키 
+        }
+    }
+}
