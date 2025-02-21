95f831db77a76f93196442c48be45dcbf2d4e81c
PARKSH0912 <kkqwert882950@gmail.com>
2025-02-21 17:21:20 +0900

    TodayTask1

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //로딩바 시작화면 
+            //게임스토리1 
+
+            Console.WriteLine("로딩바 엔터를 치면 스토리가 시작됩니다");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("xxxx.. xxxxxXX!!");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("xxㅇ.ㅏ 아아 들리세요?");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("빨리와주세요 지금 생존자가 일어났어요");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("네 네 아! 누구냐고요?");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("저는 계체명 Alice입니다. 지금 막 일어나서 정신이없으시겠지만 ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("지금은 2225년도입니다 캡슐에 있으시는동안 전쟁이 일어나서 지금 일어나시게 되었어요 ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("전쟁은 끝났냐고요?");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("아뇨 아직까지도 전쟁은 이어지고있습니다 하지만 길어진 전쟁에서 인류는 대부분 죽게되었고");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("살아남은 인류가 인형을 만들어 전쟁을 이어가고있습니다");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("인형을 만들때 사람의 피가 필요해요");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("같은 인형이라도 어떤 피가 사용되었는지에 따라 그 인형의 가치가 달라져요 ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("그러니 잠시만요 ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("푹");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("팀장님 여기와서 수치확인해보세요");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("호.. 자네 혹시 갈곳이 없다면 우리 기지인 카즈델리안에 가지않겠나? ");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("우리라면 자네를 안전하게 지켜줄수있다네");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("그나저나 이름을 안물어봤군 자네 이름은 어떻게되나?");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("플레이어");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("그래 플레이어이라고? 카즈델리안에서 잘부탁한다");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("끝");
+
+        }
+    }
+}
