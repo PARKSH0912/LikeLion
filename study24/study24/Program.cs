d173662510c6357403580c0ad5c6c5bb7666285d
PARKSH0912 <kkqwert882950@gmail.com>
2025-03-04 14:31:32 +0900

    study24

---
+++ study24/study24/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //List<int> list = new List<int>();
+
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+
+            ////foreach(int i in list)
+            ////{
+            ////    Console.WriteLine(i);
+            ////}
+
+            ////Console.WriteLine(list[1]);
+            ////list.Insert(1, 100);
+            ////Console.WriteLine(list[1]);
+
+            ////Console.WriteLine(list[0]);
+
+            //list.Insert(1, 100);
+            //Console.WriteLine(list.Count);
+
+            //list[2] = 200;
+
+            //foreach (int i in list)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            ////Stack 
+
+            //Stack stack = new Stack();
+
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+
+            //while (stack.Count > 0)
+            //{
+            //    Console.WriteLine(stack.Pop());
+            //}
+
+            //Queue queue = new Queue();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+
+            //while (queue.Count > 0)
+            //{
+            //    Console.WriteLine(queue.Dequeue());
+            //}
+
+
+            //Queue queue = new Queue();
+
+            //queue.Enqueue("q");
+            //queue.Enqueue("w");
+            //queue.Enqueue("e");
+            //queue.Enqueue("r");
+            //queue.Enqueue("점멸");
+            //queue.Enqueue("평타");
+
+            //while(queue.Count > 0)
+            //{
+            //    Console.WriteLine(queue.Dequeue());
+            //}
+
+
+            ////ArrayList 생성
+            //ArrayList arrayList = new ArrayList();
+
+            ////요소추가
+            //arrayList.Add(1);       //정수
+            //arrayList.Add("Hello"); //문자열
+            //arrayList.Add(3.14);    //실수
+
+            ////요소접근
+            //Console.WriteLine("ArrayList 요소: ");
+            //foreach(var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            ////요소 제거
+            //arrayList.Remove(1);
+            //Console.WriteLine("\n ArraryList요소 제거 후 :");
+
+            //foreach(var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Hashtable 클래스
+            //키 - 값 쌍을 저장하는 컬렉션이다
+            //키를 이용해 값을 빠르게 검색
+
+            //Hashtable hashtable = new Hashtable();
+
+            ////키-값 쌍 추가
+            //hashtable["Alice"] = 25;
+            //hashtable["Bob"] = 30;
+            //hashtable["포션"] = 20;
+
+            //Console.WriteLine("Hashtable 요소 : ");
+            //foreach(DictionaryEntry entry in hashtable)
+            //{
+            //    Console.WriteLine($"Key : {entry.Key}, Value : {entry.Value}");
+            //}
+
+            ////특정 키의 값을 가져오기
+            //Console.WriteLine($"\n Alice의 나이 : {hashtable["Alice"]}");
+
+            ////요소제거
+            //hashtable.Remove("Bob");
+
+            //Console.WriteLine("Hashtable 요소:");
+            //foreach(DictionaryEntry entry in hashtable)
+            //{
+            //    Console.WriteLine($"Key : {entry.Key}, Value : {entry.Value}");
+            //}
+
+
+            //제네릭 사용하기(Genenrics)
+            //<T> 제네릭 클래스를 사용하면 특정 타입에 종속되지 않는 범용클래스를 만들수있습니다
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
