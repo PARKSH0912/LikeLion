bf7b5519fbe5809f60294018d4c46a42d1f575e0
PARKSH0912 <kkqwert882950@gmail.com>
2025-03-06 11:04:57 +0900

    study40

---
+++ study40/study40/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study40
+{
+    //부모클래스의 생성자 호출
+
+    class Parent
+    {   
+        
+        public Parent(string message)
+        {
+            Console.WriteLine("부모생성자 " + message); //2실행 
+        }
+
+    }
+
+    class Child : Parent
+    {
+
+        public Child() : base("하하하성공") //부모 생성자호출 , 1실행
+        {
+            Console.WriteLine("자식 생성자 호출"); // 3실행
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Child child = new Child();
+
+        }
+    }
+}
