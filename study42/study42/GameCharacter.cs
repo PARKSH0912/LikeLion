42974d2decb5761523eee5afa1acb77af4839412
PARKSH0912 <kkqwert882950@gmail.com>
2025-03-06 13:04:52 +0900

    study42

---
+++ study42/study42/GameCharacter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study42
+{
+    public abstract class GameCharacter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+
+        protected GameCharacter(string name, int health, int attack, int defence)
+        {
+            Name = name;
+            Health = health;
+            Attack = attack;
+            Defence = defence;
+        }
+
+        //추상 메서드 : 모든 캐릭터가 구현해야 하는 기본 공격
+
+        public abstract void BasicAttack(GameCharacter Target);
+
+        //추상메서드 : 모든 캐릭터가 구현해야하는 특수공격
+
+        public abstract void SpecialAttack(GameCharacter Target);
+
+
+
+        //일반메서드 : 모든캐릭터가 공유하는 기능
+
+        public void TakeDamage(int damage)
+        {
+            int actualDamae = Math.Max(1, damage - Defence);
+
+            Health = Math.Max(0, Health - actualDamae);
+
+            Console.WriteLine($"{Name}이 {actualDamae}의 피해를 받았습니다. 남은체력 : {Health}");
+
+        }
+
+
+    }
+}
---
+++ study42/study42/Mage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study42
+{
+    public class Mage : GameCharacter
+    {
+        public Mage(string name) : base(name, 80, 20, 5)
+        {
+
+        }
+
+        public override void BasicAttack(GameCharacter Target)
+        {
+            Console.WriteLine($"{Name}이 {Target.Name}에게 마법 구체를 던집니다!");
+            Target.TakeDamage(Attack);
+        }
+        public override void SpecialAttack(GameCharacter Target)
+        {
+            Console.WriteLine($"{Name}이 {Target.Name}에게 화염폭발을 시전합니다!");
+            Target.TakeDamage(Attack*2);
+
+        }
+
+
+
+    }
+}
