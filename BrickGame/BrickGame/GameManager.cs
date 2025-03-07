5660fc141b2a25b06554ab2468db9a7385be8302
PARKSH0912 <kkqwert882950@gmail.com>
2025-03-07 13:13:00 +0900

    BrickGame

---
+++ BrickGame/BrickGame/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickGame
+{
+    class GameManager
+    {
+
+        Ball m_pBall = null;
+
+        public void Initialize()
+        {
+            if (m_pBall == null)
+            {
+                m_pBall = new Ball();
+                m_pBall.Initialize();
+            }
+        }
+        public void Progress()
+        {
+            m_pBall.Progress();
+        }
+        public void Rendor()
+        {
+            Console.Clear();
+            m_pBall.Render();
+        }
+        public void Release()
+        {
+            m_pBall.Release();
+        }
+
+    }
+}
